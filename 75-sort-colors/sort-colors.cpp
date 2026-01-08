4d7b951104b4e67820952927423f5bd6228ac261
Gouthami Vadlamudi <vadlamudigouthami04@gmail.com>
2026-01-08 22:47:01 +0530

    Time: 0 ms (100.00%) | Memory: 11.7 MB (46.93%) - LeetSync

---
+++ 75-sort-colors/sort-colors.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int cnt0=0,cnt1=0,cnt2=0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==0){
+                cnt0++;
+            }
+            else if(nums[i]==1){
+                cnt1++;
+            }
+            else if(nums[i]==2)
+            {
+                cnt2++;
+            }
+                
+            
+        }
+        int idx=0;
+        for(int i=0;i<cnt0;i++){
+            nums[idx++]=0;
+        }
+        for(int i=0;i<cnt1;i++){
+            nums[idx++]=1;
+        }
+         for(int i=0;i<cnt2;i++){
+            nums[idx++]=2;
+        }
+        
+        
+    }
+};
