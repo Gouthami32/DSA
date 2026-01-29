152dcae43c3a09ec0f81a1ff303aed8464135760
Gouthami Vadlamudi <vadlamudigouthami04@gmail.com>
2026-01-29 21:21:13 +0530

    Time: 0 ms (100.00%) | Memory: 11.8 MB (33.02%) - LeetSync

---
+++ 979-di-string-match/di-string-match.cpp
@@ -0,0 +1,25 @@
+#include <vector>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> diStringMatch(string s) {
+        int n = s.size();
+        int low = 0, high = n;
+        vector<int> perm;
+
+        for (char c : s) {
+            if (c == 'I') {
+                perm.push_back(low++);
+            } else { // 'D'
+                perm.push_back(high--);
+            }
+        }
+
+        // One number will be left
+        perm.push_back(low);  
+
+        return perm;
+    }
+};
