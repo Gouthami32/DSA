41862721cf3815aac57f2ebcbf7be41d104480dd
Gouthami Vadlamudi <vadlamudigouthami04@gmail.com>
2026-07-09 21:53:21 +0530

    Time: 0 ms (100.00%) | Memory: 28.4 MB (10.10%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -3,23 +3,15 @@ public:
     int majorityElement(vector<int>& nums) {
 
         int n = nums.size();
-
-        for(int i = 0; i < n; i++) {
-
-            int count = 0;
-
-            for(int j = 0; j < n; j++) {
-
-                if(nums[i] == nums[j]) {
-                    count++;
-                }
-            }
-
-            if(count > n / 2) {
-                return nums[i];
+        unordered_map<int,int>mp;
+        for(int i=0;i<n;i++){
+            mp[nums[i]]++;
+        }
+        for(auto x:mp){
+            if(x.second>n/2){
+                return x.first;
             }
         }
-
         return -1;
     }
 };
