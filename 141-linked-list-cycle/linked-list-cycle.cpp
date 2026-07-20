17bc5dbab031b5092c0db1598e1d716c85346dfe
Gouthami Vadlamudi <vadlamudigouthami04@gmail.com>
2026-07-20 22:05:37 +0530

    Time: 14 ms (10.70%) | Memory: 15.2 MB (5.88%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.cpp
+++ 141-linked-list-cycle/linked-list-cycle.cpp
@@ -9,16 +9,15 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        ListNode* slow=head;
-        ListNode* fast=head;
-        while(fast!=NULL && fast->next!=NULL){
-            slow=slow->next;
-            fast=fast->next->next;
-            if(slow==fast){
-                return true;
-            }
-        
+       unordered_map<ListNode*,int>mp;
+       ListNode* temp=head;
+       while(temp!=NULL){
+        if(mp.find(temp)!=mp.end()){
+            return true;
         }
-        return false;
+         mp[temp] = 1;
+        temp=temp->next;
+       }
+       return false;
     }
 };
